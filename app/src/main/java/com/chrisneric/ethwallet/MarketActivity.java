a9405d350e930ee6035831adba8bbcaaf1be2160
zds8823 <36308993+zds8823@users.noreply.github.com>
2018-03-30 20:16:01 -0300

    Added the send menu and Market menu also added buttons and text and input layouts to both
    however having trouble with constraints on the main menu the error
     causes the buttons to jump to the top of the page
    
    Cleared all errors 10 warnings about labels

--- app/src/main/java/com/chrisneric/ethwallet/MarketActivity.java
+++ app/src/main/java/com/chrisneric/ethwallet/MarketActivity.java
@@ -5,18 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class WalletMenu extends AppCompatActivity {
+public class MarketActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wallet_menu);
+        setContentView(R.layout.activity_market_menu);
+    }
+    public void MainActivity (View v){
+        startActivity(new Intent(MarketActivity.this, MainActivity.class));
     }
 
-    public void MainActivity (View v){
-        startActivity(new Intent(WalletMenu.this, MainActivity.class));
-    }
-    public void SendActivity (View v){
-        startActivity(new Intent(WalletMenu.this, SendActivity.class));
+    public void WalletMenu (View v){
+        startActivity(new Intent(MarketActivity.this, WalletMenu.class));
     }
 }
